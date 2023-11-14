a9dbca809fdae9080e9bc61aededa2bc2945aecd
limseungchae <tmdco12@naver.com>
2023-11-14 11:46:45 +0900

    수정

--- src/main/java/com/example/custom/service/CustomService.java
+++ src/main/java/com/example/custom/service/CustomService.java
@@ -15,124 +15,117 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+// Spring의 Service로 선언하여 bean으로 관리합니다
 @Service
+// Lombok을 통한 생성자 주입을 사용합니다
 @RequiredArgsConstructor
 public class CustomService {
 
+    // Custom 및 Account Repository를 주입받습니다
     private final CustomRepository customRepository;
     private final AccountRepository accountRepository;
 
-    // 등록
+    // 등록 기능
     @Transactional
     public Header<Custom> create(Header<Custom> request) {
-        Custom custom = request.getData(); // 요청에서 데이터를 가져옴
-        Custom savedCustom = customRepository.save(custom); // Custom 엔티티 저장
-
-        return Header.OK(savedCustom); // 저장된 엔티티를 응답
+        // 요청에서 Custom 데이터를 가져옵니다
+        Custom custom = request.getData();
+        // Custom 엔티티를 저장하고 저장된 엔티티를 반환합니다
+        Custom savedCustom = customRepository.save(custom);
+        return Header.OK(savedCustom);
     }
 
-    // 수정
-    @Transactional
-    public Header<Custom> update(Header<Custom> request) {
-        Custom custom = request.getData(); // 요청에서 데이터를 가져옴
-        Custom savedCustom = customRepository.save(custom); // Custom 엔티티 수정 후 저장
-
-        return Header.OK(savedCustom); // 수정된 엔티티를 응답
-    }
-
-    // 상세정보
+    // 상세정보 조회
     @Transactional
     public Header<Custom> read(String busiNum) {
+        // 사업자 번호 길이가 20이 아닌 경우 처리
         if (busiNum.length() != 20) {
-            // busiNum 길이가 20이 아닌 경우에 대한 처리
+            // 처리 로직 작성
+        }
+        // 주어진 사업자 번호에 해당하는 Custom 엔티티를 조회합니다
+        Custom custom = customRepository.findByBusiNum(busiNum);
+        // 조회된 Custom 엔티티를 반환합니다
+        return Header.OK(custom);
+    }
+
+    // 수정 기능
+    @Transactional
+    public Header<Custom> update(Header<Custom> request) {
+        // 요청에서 Custom 데이터를 가져옵니다
+        Custom custom = request.getData();
+        // Custom 엔티티를 수정하고 저장된 엔티티를 반환합니다
+        Custom savedCustom = customRepository.save(custom);
+        return Header.OK(savedCustom);
+    }
+
+    // 삭제 기능
+    public Header delete(String busiNum) {
+        // 사업자 번호 길이가 20이 아닌 경우 처리
+        if (busiNum.length() != 20) {
+            // 처리 로직 작성
         }
 
-        Custom custom = customRepository.findByBusiNum(busiNum); // 특정 busiNum을 가진 Custom 조회
+        // 주어진 사업자 번호에 해당하는 Custom 및 Account 엔티티를 조회합니다
+        Optional<Custom> optionalCustom = customRepository.getByBusiNum(busiNum);
+        Optional<Account> optionalAccount = accountRepository.getByBusiNum(busiNum);
 
-        if (custom == null) {
-            // Custom 엔티티를 찾을 수 없는 경우에 대한 처리
-        }
-
-        return Header.OK(custom); // 조회된 Custom 엔티티 반환
+        // Custom 엔티티가 존재하는 경우 삭제를 수행합니다
+        return optionalCustom.map(custom -> {
+            customRepository.delete(custom);
+            // Account 엔티티가 존재하는 경우 Account 엔티티도 삭제를 수행합니다
+            return optionalAccount.map(account -> {
+                accountRepository.delete(account);
+                // 삭제 완료를 응답합니다
+                return Header.OK();
+            }).orElseGet(() -> Header.ERROR("Account 정보를 찾을 수 없음"));
+        }).orElseGet(() -> Header.ERROR("Custom 정보를 찾을 수 없음"));
     }
 
     // 검색조회
     public List<Custom> searchList(Header<Custom> request) {
-        List<Custom> customerList = searchCustomList(request); // Custom 검색 조건에 따라 조회
+        // Custom 검색 조건에 따라 엔티티를 조회합니다
+        List<Custom> customerList = searchCustomList(request);
 
+        // 조회된 결과가 없는 경우 빈 리스트 반환
         if (customerList == null) {
-            return new ArrayList<>(); // null일 경우 빈 리스트 반환
+            return new ArrayList<>();
         }
 
+        // 일부 필드로 이루어진 Custom 엔티티 리스트 반환
         return customerList.stream()
                 .map(custom -> Custom.builder()
                         .busiNum(custom.getBusiNum())
                         .custom(custom.getCustom())
                         .build())
-                .collect(Collectors.toList()); // Custom 엔티티의 일부 필드로 구성된 리스트 반환
+                .collect(Collectors.toList());
     }
 
-    // 삭제
-    public Header delete(String busiNum) {
-        if (busiNum.length() != 20) {
-            // busiNum 길이가 20이 아닌 경우에 대한 처리
-        }
-
-        Optional<Custom> optionalCustom = customRepository.getByBusiNum(busiNum); // 특정 busiNum을 가진 Custom 조회
-        Optional<Account> optionalAccount = accountRepository.getByBusiNum(busiNum); // 특정 busiNum을 가진 Account 조회
-
-        return optionalCustom.map(custom -> {
-            customRepository.delete(custom); // Custom 엔티티 삭제
-            return optionalAccount.map(account -> {
-                accountRepository.delete(account); // Account 엔티티 삭제
-                return Header.OK(); // 삭제 완료 응답
-            }).orElseGet(() -> Header.ERROR("Account information not found")); // Account가 없을 경우 응답
-        }).orElseGet(() -> Header.ERROR("Custom information not found")); // Custom이 없을 경우 응답
-    }
-
-    // 조회
+    // 전체 목록 조회
     public List<Custom> list() {
-        return customRepository.findAll(); // 모든 Custom 엔티티 목록 조회
+        // 모든 Custom 엔티티를 조회하여 반환합니다
+        return customRepository.findAll();
     }
 
-
-
-    // 검색기능
-    /**
-     * 사업자 번호에 대한 Specification을 생성합니다.
-     *
-     * @param busiNum 검색할 사업자 번호
-     * @return 사업자 번호를 기준으로 하는 Specification
-     */
+    // 사양 조건에 따른 Custom 검색을 수행하는 메서드
     public static Specification<Custom> equalBusiNum(String busiNum) {
         return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("busiNum"), busiNum);
     }
 
-    /**
-     * 거래처명에 대한 Specification을 생성합니다.
-     *
-     * @param custom 검색할 거래처명
-     * @return 거래처명을 기준으로 하는 Specification
-     */
+    // 사양 조건에 따른 Custom 검색을 수행하는 메서드
     public static Specification<Custom> equalCustom(String custom) {
         return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("custom"), custom);
     }
 
-    /**
-     * Header 내 Custom 데이터를 활용하여 조건 검색을 수행합니다.
-     *
-     * @param custom Header 내의 Custom 데이터
-     * @return 조건에 부합하는 Custom 엔티티 리스트
-     */
-    public List<Custom> searchCustomList(Header<Custom> custom){
+    // Custom 검색을 수행하는 메서드
+    public List<Custom> searchCustomList(Header<Custom> custom) {
         Specification<Custom> spec = null;
 
         // 사업자 번호에 따른 검색 조건
-        if(custom.getData().getBusiNum() != null){
+        if (custom.getData().getBusiNum() != null) {
             String busiNum = custom.getData().getBusiNum().trim();
 
             // 사업자 번호에 대한 Specification 추가
-            if(spec == null){
+            if (spec == null) {
                 spec = Specification.where(CustomService.equalBusiNum(busiNum));
             } else {
                 spec = spec.or(CustomService.equalBusiNum(busiNum));
@@ -140,19 +133,16 @@ public class CustomService {
         }
 
         // 거래처명에 따른 검색 조건
-        if(custom.getData().getCustom() != null){
+        if (custom.getData().getCustom() != null) {
             // 거래처명에 대한 Specification 추가
-            if(spec == null){
+            if (spec == null) {
                 spec = Specification.where(CustomService.equalCustom(custom.getData().getCustom().trim()));
             } else {
                 spec = spec.or(CustomService.equalCustom(custom.getData().getCustom().trim()));
             }
         }
 
-        // Specification을 이용하여 Custom 엔티티를 검색합니다.
+        // Specification을 이용하여 Custom 엔티티를 검색합니다
         return customRepository.findAll(spec);
     }
 }
-
-
-
